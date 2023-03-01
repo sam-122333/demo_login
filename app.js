9fa276828632d44f28b95cb676ebdc02f99b3889
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-03-01 16:55:42 +0530

    33

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ app.use(
 // );
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "script-src 'self' cdn.jsdelivr.net"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
