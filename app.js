65ba10a1ad2e6f070f519898283bf594db533de6
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-03-01 10:28:26 +0530

    commit 8

--- app.js
+++ app.js
@@ -37,6 +37,14 @@ app.use(helmet());
 // linked the routes in app.js
 app.use(require("./routes/auth"));
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "connect-src 'self' http://localhost:4000"
+  );
+  next();
+});
+
 //server the front end
 app.use(express.static(path.join(__dirname, "./client/build")));
 
