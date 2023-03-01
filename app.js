53d1667621517fb2092e6ab74d6b0c580e9fc581
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-03-01 09:05:49 +0530

    insert client build

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path");
 
 //set up express
 const app = express();
@@ -36,13 +37,17 @@ app.use(helmet());
 // linked the routes in app.js
 app.use(require("./routes/auth"));
 
+//server the front end
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html")),
+    function (err) {
+      res.status(err);
+    };
+});
+
 const PORT = process.env.PORT || 4000;
-
-// set heroku
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 app.listen(PORT, "localhost", () => {
   console.log(`server listen on port 4000`);
