0f46059603f5a00a74ce1a87a21284a24910cf38
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-03-01 09:29:50 +0530

    commit 2

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(require("./routes/auth"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html")),
+  res.sendFile(path.join(__dirname,"build", "./client/build/index.html")),
     function (err) {
       res.status(err);
     };
